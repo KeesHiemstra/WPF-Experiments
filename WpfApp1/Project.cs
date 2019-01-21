54ea3da5a415f13b7ca6ec188d64eae7a486f07c
Kees Hiemstra <chi@xs4all.nl>
2019-01-21 08:59:51 +0100

    Moved to the dev folder

---
+++ WpfApp1/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+  public class Project
+  {
+    public string Reference { get; set; }
+
+    public string Name { get; set; }
+
+    public List<string> Tags { get; set; }
+  }
+}
